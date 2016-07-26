d94d06a35d498c5b6aa09a8124849132aeb7f415
rafinkarki <rafinkarki@outlook.com>
2016-07-26 20:59:46 +0545

    changes

--- inc/so-widgets/graph/graph.php
+++ inc/so-widgets/graph/graph.php
@@ -11,17 +11,20 @@ class Fi_Print_Graph_Widget extends SiteOrigin_Widget {
 				),
 			array(),
 			array(
+				'type' => array(
+					'type' => 'select',
+					'label' => __('Graph Type Icon Style', 'siteorigin-widgets'),
+					'options' => array(
+						'line' => __('Line Graph Style', 'siteorigin-widgets'),
+						'bar' => __('Bar Graph Style', 'siteorigin-widgets'),						
+					)
+				),			
 
-				'title' => array(
-					'type' => 'text',
-					'label' => __('Tilte', 'siteorigin-widgets'),
+				'x_axis_value' => array(
+					'type'  => 'text',
+					'label' => __( 'Value corresponds to X axis', 'fi_print' ),
+					'description' => __( 'Provide the set of values seperated by commas. eg:1st Jan,2nd Jan,3rd Jan,4th Jan', 'fi_print' )
 				),
-
-				'sub_title' => array(
-					'type' => 'text',
-					'label' => __( 'Sub Title', 'fi_print' ),
-				),
-
 				'graph_repeater' => array(
 					'type'  => 'repeater',
 					'label' => __('Enter Graph Details.', 'fi_print'),
@@ -30,20 +33,20 @@ class Fi_Print_Graph_Widget extends SiteOrigin_Widget {
 							'type'  => 'text',
 							'label' => __( 'Graph Label', 'fi_print' )
 						),
-						'x_axis_value' => array(
-							'type'  => 'text',
-							'label' => __( 'Value corresponds to X axis', 'fi_print' )
-						),
 						'y_axis_value' => array(
 							'type'  => 'text',
-							'label' => __( 'Value corresponds to Y axis', 'fi_print' )
+							'label' => __( 'Value corresponds to Y axis', 'fi_print' ),
+							'description' => __( 'Provide the set of values seperated by commas. eg:1,2,3,4', 'fi_print' )
 						),
-						'background_color' => array(
-							'type'  => 'color',
-							'label' => __( 'Background Color', 'fi_print' )
-						),
-
-						
+						'color' => array(
+							'type' => 'select',
+							'label' => __('Select Color', 'siteorigin-widgets'),
+							'options' => array(
+								'red' => __('Red', 'siteorigin-widgets'),
+								'yellow' => __('Yellow', 'siteorigin-widgets'),						
+								'blue' => __('Blue', 'siteorigin-widgets'),						
+							)
+						),						
 					),
 				),
 
--- inc/so-widgets/graph/tpl/default.php
+++ inc/so-widgets/graph/tpl/default.php
@@ -1,45 +1,45 @@
-
-            <div class="col-xs-12 col-md-6">
-              <canvas id="chart-area"></canvas>
-            </div>
-
+<div class="col-xs-12 col-md-6">
+  <canvas id="chart-area"></canvas>
+</div>
+<?php 
+  $type=esc_attr($instance['type']);
+  $count=count($instance['graph_repeater']);
+  $x_axis=wp_kses_post($instance['x_axis_value']);
+  $x_axis = explode(",", $x_axis);
+  foreach($x_axis as $k=>$value):
+    $x_value .=  ($k==0) ? '["'.$value.'"' : ', "'.$value.'"';
+  endforeach;
+  $x_value=$x_value."]";
+  foreach($instance['graph_repeater'] as $key=>$graph):
+    ++$key;
+    $label[$key]=esc_attr($graph['graph_label']);
+    $y_value[$key]='['.$graph['y_axis_value'].']';
+    $color[$key]=$graph['color'];
+  endforeach;
+  $datasets='[';
+  for($i=1;$i<=$count;$i++){
+    $datasets .=  ($i==1) ? '{label:"'.$label[$i].'",backgroundColor:chartColors.'.$color[$i].',data:'.$y_value[$i].'}' : ', {label:"'.$label[$i].'",backgroundColor:chartColors.'.$color[$i].',data:'.$y_value[$i].'}';
+  }
+  $datasets.=']';
+?>
 <script type="text/javascript">
 jQuery(document).ready(function () {
 
   if (typeof Chart != "undefined") {
-
+    var count=<?php echo $count;?>;
     var areaChartData = {
-      labels: ["20th Jan", "21th Jan", "22th Jan", "23th Jan", "24th Jan", "25th Jan"],
-      datasets: [{
-        label: "Personal",
-        backgroundColor: chartColors.red,
-        data: [5, 24, 17, 10, 21, 38]
-      }, {
-        label: "Business1",
-        backgroundColor: chartColors.blue,
-        data: [18, 10, 4, 20, 30, 42]
-      },
-      {
-        label: "sudeep",
-        backgroundColor: chartColors.blue,
-        data: [18, 10, 4, 20, 30, 42]
-      }]
+      labels: <?php echo $x_value;?>,
+
+      datasets:<?php echo $datasets;?>
     };
 
     if (jQuery('#chart-area').length) {
       new Chart(jQuery('#chart-area'), {
-        type: 'line',
+        type: '<?php echo $type;?>',
         data: areaChartData,
         options: chartOptions
       });
     }
-
-
   }
-
-
 });
-
-
-
 </script>
