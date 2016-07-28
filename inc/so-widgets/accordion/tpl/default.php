7ee17a30f58143a77ac944ea2008a06677403261
rafinkarki <rafinkarki@outlook.com>
2016-07-28 18:42:37 +0545

    chages

--- inc/so-widgets/accordion/tpl/default.php
+++
@@ -1,9 +0,0 @@
-<ul class="faq">
-    <?php   foreach ( $instance['accordion_repeater'] as $key=>$accordion ){  
-        $active=$accordion['active']?'open':'';?>
-        <li class="<?php echo $active;?>">
-	        <h6><?php echo esc_attr( $accordion['title'] );   ?></h6>
-			<p class="faq-body"><?php echo wp_kses_post($accordion['textarea']);?></p>
-		</li>
-    <?php }?>
-</ul>
