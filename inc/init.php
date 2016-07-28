93a8ad863d9030cf9f8c71d759f47e6b93d2960c
sudeep22169 <lumbinibhw@gmail.com>
2016-07-28 18:50:45 +0545

    fix init

--- inc/init.php
+++ inc/init.php
@@ -4,29 +4,32 @@
  */
 if ( class_exists( 'SiteOrigin_Widget' ) ) {
 
- $so_widgets = array(
-  'slider'           => 'slider/slider.php',
-  'services'         =>'services/services.php',
-  'skill'            =>'skill/skill.php',
-  'title'            => 'title/title.php',
-  'counter'          => 'counter/counter.php',
-  'testimonial'      => 'testimonial/testimonial.php',
-  'client'           =>'client/client.php',
-  'post'             =>'post/post.php',
-  'address'          =>'address/address.php',
-  'graph'            => 'graph/graph.php',
-  'highlights'       =>'company-highlights/highlights.php',
-  'sliding-services' =>'sliding-services/sliding-services.php',
-  'tabs'  =>'tabs/tabs.php',
-  'form-map'  =>'form-map/form-map.php',
-  'download-btn'  =>'download-btn/download-btn.php',
- );
+	$so_widgets = array(
+		'slider'           => 'slider/slider.php',
+		'services'         =>'services/services.php',
+		'skill'            =>'skill/skill.php',
+		'title'            => 'title/title.php',
+		'counter'          => 'counter/counter.php',
+		'testimonial'      => 'testimonial/testimonial.php',
+		'client'           =>'client/client.php',
+		'post'             =>'post/post.php',
+		'address'          =>'address/address.php',
+		'graph'            => 'graph/graph.php',
+		'highlights'       =>'company-highlights/highlights.php',
+		'sliding-services' =>'sliding-services/sliding-services.php',
+		'tabs'             =>'tabs/tabs.php',
+		'contact-ways'     =>'contact-ways/contact_ways.php',
+		'hero'=>'hero/hero.php',
+		'form-map'		=>'form-map/form-map.php',
+		'download-btn'		=>'download-btn/download-btn.php',
 
- $temp_dir = get_template_directory();
- foreach ($so_widgets as $key => $value) {
+	);
 
-  require $temp_dir . '/inc/so-widgets/'. $value;
+	$temp_dir = get_template_directory();
+	foreach ($so_widgets as $key => $value) {
 
- }
+		require $temp_dir . '/inc/so-widgets/'. $value;
+
+	}
 
 }
